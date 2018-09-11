e14a15d20c290e59144ed9fb56aa8272bf90f57c
Oleg Golovchenko <IgRoK_be@hotmail.com>
2018-09-11 21:58:04 +0200

    added some basic ui for flyingball .net game

---
+++ FlyingBall.NET/GameVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlyingBall.NET
+{
+    public class GameVM:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _realScore = 0;
+        private string _score;
+
+        public string Score
+        {
+            get
+            {
+                return _score;
+            }
+            private set
+            {
+                if (_score != value)
+                {
+                    _score = value;
+                    RaisePropertyChanged(nameof(Score));
+                }
+            }
+        }
+
+
+        public GameVM()
+        {
+            AugmentScore();
+        }
+
+        public ICommand AugmentScoreCommand
+        {
+            get
+            {
+                return new DelegateCommand(AugmentScore);
+            }
+        }
+
+        private void AugmentScore()
+        {
+            Score = _realScore.ToString("0000000");
+            _realScore++;
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
